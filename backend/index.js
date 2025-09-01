5f064024cfc02b8f3b79c3f9697ddb4312760d5b
Tharindu Nuwantha <105153249+TharinduNuwantha@users.noreply.github.com>
2025-09-01 17:24:52 +0530

    config

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
